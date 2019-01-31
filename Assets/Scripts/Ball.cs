d99d074081b4ffbde4fd16b67b04892de20dc675
Peyu <trancoz@gmail.com>
2019-01-31 11:56:34 -0300

    Movimiento de paleta y pelota

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+
+    public float ballInitialVelocity = 600f;
+
+
+    private Rigidbody rb;
+    private bool ballInPlay;
+    public Vector3 vel;
+
+    void Awake() //se llama antes que el start
+    {
+
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    //Se usa update y no fixedupdate porque no se desea que se updatee la velocidad todo el tiempo.
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+        {
+            transform.parent = null; //la paleta deja de ser el padre para que pueda moverse en libertad
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+        }
+    
+        if (ballInPlay == true) {
+            vel = rb.velocity;    
+        }
+
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public float paddleSpeed = 1f;
+    
+    private Vector3 playerPos = new Vector3(0, -9.5f, 0);
+
+    void Update()
+    {
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        //Mathf.Clamp setea un valor entre un minimo y un maximo, en este caso la plaeta no va a poder ir mas alla de 8 y -8
+        //new vector (x,y,z).  EN este caso no quiero que se mueva sobre ejes Y ni Z, siempre tendran esos valores
+        playerPos = new Vector3(Mathf.Clamp(xPos, -7.5f, 7.5f), -9.5f, 0f);
+        transform.position = playerPos;
+
+    }
+}
